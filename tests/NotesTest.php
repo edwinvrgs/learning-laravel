e01745038a80a536edb7069a772de7b0e6203696
Edwin Vargas <onix.edwin18@gmail.com>
2016-06-08 17:12:22 -0430

    Some changes

---
+++ tests/NotesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Note;
+
+class NotesTest extends TestCase
+{
+    public function test_notes_list()
+    {
+        //Having
+        Note::create(['note' => 'My first note']);
+        Note::create(['note' => 'Second note']);
+
+        //When
+        $this->visit('notes')
+            //Then
+            ->see('My first note')
+            ->see('Second note');
+    }
+}
